e42cf87ece7706264b9cb108975ba64725fc8890
ninhatzo <anac.estudantes2@gmail.com>
2025-08-18 19:45:09 -0300

    Criando a atividade 4

---
+++ atividade4/js/main.js
@@ -0,0 +1,27 @@
+function validaProc() {
+
+    // Declarando vetor
+    let valor=[];
+    let valorImpar = [];
+    let contador=0;
+
+    // Alimentando o vetor com dados
+    for(let i = 0; i<10;i++) {
+
+        valor[i]=parseFloat(prompt("Digite o " +(i)+ "º número: ").replace(",", "."));
+        console.log( (i+1)+ "º número informado é: " + valor[i]);
+    }
+
+    console.log("")
+    console.log("Números ímpares:")
+
+    // Conta
+    for(let i = 0; i<10;i++) {
+
+        if (i % 2) {
+            valorImpar[i] = valor[i]
+        console.log("Posição "+(i)+ " = " +valorImpar[i])
+        }
+     }
+    return false;
+}
